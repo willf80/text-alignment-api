d367aa2917523776c83b16e5eab6954f435c7e8c
WILLY Falone <willyfkouadio@gmail.com>
2021-08-02 00:28:01 +0200

    ajoute le scénario de test de la fonction wrapText

---
+++ test/core/utils/text-transform.util.spec.ts
@@ -0,0 +1,31 @@
+
+const lineMax = 80;
+
+describe('quand on passe un texte', () => {
+  // let text = '';
+  describe('et que le texte est vide', () => {
+    it(`devrait retourner un texte vide`, () => {
+      //
+    });
+  });
+
+  describe('et que le texte est sur une ligne', () => {
+    describe(`et que le texte fait moins de ${lineMax} caractères`, () => {
+      it(`devrait retourner la même ligne`, () => {
+        //
+      });
+    });
+
+    describe(`et que le texte fait plus de ${lineMax} caractères`, () => {
+      it(`devrait retourner un texte sur plusieurs ligne`, () => {
+        //
+      });
+    });
+  });
+
+  describe('et que le texte est sur plusieurs ligne', () => {
+    it(`devrait retourner un texte sur plusieurs ligne`, () => {
+      //
+    });
+  });
+});

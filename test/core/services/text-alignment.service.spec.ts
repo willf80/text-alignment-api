05290a30297ba4a43aedea11a336c5cf55e14f0d
WILLY Falone <willyfkouadio@gmail.com>
2021-08-03 00:42:28 +0200

    wip(text-alignment): scenario de tests de <<justify()>>

--- test/core/services/text-alignment.service.spec.ts
+++ test/core/services/text-alignment.service.spec.ts
@@ -1,3 +1,19 @@
 describe('quand on passe un texte à justifier', () => {
-  //
+  describe('et que le texte contient un seul mot', () => {
+    //
+  });
+
+  describe('et que le texte fait moins de 80 caractères', () => {
+    describe(`et que le texte est sur une ligne`, () => {
+      //
+    });
+
+    describe(`et que le texte est sur plusieurs lignes`, () => {
+      //
+    });
+  });
+
+  describe(`et que le texte est sur plusieurs lignes`, () => {
+    //
+  });
 });

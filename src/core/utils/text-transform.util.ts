3aebddb51aa8d1d325a96fd8eef2538046af45bd
WILLY Falone <willyfkouadio@gmail.com>
2021-08-02 23:51:50 +0200

    wip(text-transform): retourne le texte avec pas plus de  80 caractères par ligne

--- src/core/utils/text-transform.util.ts
+++ src/core/utils/text-transform.util.ts
@@ -1,17 +1,37 @@
-import { TextOptions } from "./text-options.enum";
+import { TextOptions } from './text-options.enum';
 
 export function wrapText(text: string, maxLine: number): string {
   if (maxLine < TextOptions.MinLine) {
-    throw new Error(`maxLine must be greater than or equal to ${TextOptions.MinLine}`);
+    throw new Error(
+      `maxLine must be greater than or equal to ${TextOptions.MinLine}`,
+    );
   }
 
-  if (!text?.trim()) {
-    return text;
-  }
+  if (!text?.trim() || text.trim().length <= TextOptions.MaxLine) return text;
 
-  if (text.trim().length <= TextOptions.MaxLine) {
-    return text;
-  }
+  const lines = text
+    .trim()
+    .split(`\n`)
+    .map((t) => t.trim());
 
-  return '';
+  const words = lines.map((line) => {
+    if (line.length <= TextOptions.MaxLine) {
+      return line;
+    }
+
+    const lineWords = line.split(` `).map(w => w.trim()).filter((word) => word);
+    let start = 0;
+    let wrapContent = '';
+    for (let end = 1; end <= lineWords.length; end++) {
+        const chunk = lineWords.slice(start, end).join(' ').length;
+        if (chunk > TextOptions.MaxLine) {
+          wrapContent += lineWords.slice(start, end-1).join(' ') + '\n';
+          start = end - 1;
+        }
+    }
+
+    return wrapContent + lineWords.slice(start).join(' ');
+  }).map(line => line.trim());
+
+  return words.join(`\n`);
 }
--- test/core/utils/text-transform.util.spec.ts
+++ test/core/utils/text-transform.util.spec.ts
@@ -1,13 +1,13 @@
 import { TextOptions } from '../../../src/core/utils/text-options.enum';
 import { wrapText } from '../../../src/core/utils/text-transform.util';
 
-describe('quand on passe un texte', () => {
+describe('quand on passe un texte et une limite de caractères par ligne', () => {
   let text = '';
   describe('et que le texte est vide', () => {
     it(`devrait retourner un texte vide`, () => {
       text = '';
       const expectedResult = wrapText(text, TextOptions.MaxLine);
-      expect(text).toBe(expectedResult);
+      expect(expectedResult).toBe(text);
     });
   });
 
@@ -17,9 +17,7 @@ describe('quand on passe un texte', () => {
       try {
         wrapText(text, line);
       } catch (e) {
-        expect(e.message).toBe(
-          `maxLine must be greater than or equal to ${TextOptions.MinLine}`,
-        );
+        expect(`maxLine must be greater than or equal to ${TextOptions.MinLine}`).toBe(e.message);
       }
     });
   });
@@ -29,20 +27,56 @@ describe('quand on passe un texte', () => {
       it(`devrait retourner la même ligne`, () => {
         text = 'Longtemps, je me suis couché de bonne heure.';
         const expectedResult = wrapText(text, TextOptions.MaxLine);
-        expect(text).toBe(expectedResult);
+        expect(expectedResult).toBe(text);
       });
     });
 
     describe(`et que le texte fait plus de ${TextOptions.MaxLine} caractères`, () => {
+      beforeEach(() => {
+        text = 
+        `Longtemps, je me suis couché de bonne heure. Parfois, à peine ma bougie éteinte, mes yeux se fermaient si vite que je n’avais pas le temps de me dire: «Je m’endors.» Et, une demi-heure après, la pensée qu’il était temps de chercher le sommeil m’éveillait; je voulais poser le volume que je croyais avoir dans les mains.`;
+      });
+
       it(`devrait retourner un texte sur plusieurs ligne`, () => {
-        //
+        const mockResult = 
+`Longtemps, je me suis couché de bonne heure. Parfois, à peine ma bougie éteinte,
+mes yeux se fermaient si vite que je n’avais pas le temps de me dire: «Je
+m’endors.» Et, une demi-heure après, la pensée qu’il était temps de chercher le
+sommeil m’éveillait; je voulais poser le volume que je croyais avoir dans les
+mains.`;
+
+        const result = wrapText(text, TextOptions.MaxLine);
+        expect(result).toBe(mockResult);
       });
     });
   });
 
   describe('et que le texte est sur plusieurs ligne', () => {
+    beforeEach(() => {
+      text = 
+`Il me semblait que j’étais moi-même ce dont parlait l’ouvrage: une église, un quatuor, la rivalité de François Ier et de Charles-Quint. 
+
+Cette croyance survivait pendant quelques secondes à mon réveil; elle ne choquait pas ma raison, mais pesait comme des écailles sur mes yeux et les empêchait de se rendre compte que le bougeoir n’était plus allumé. 
+  Puis elle commençait à me devenir inintelligible, comme après la métempsycose les pensées d’une existence antérieure; le sujet du livre se détachait de moi, j’étais libre de m’y appliquer ou non; aussitôt je recouvrais la vue et j’étais bien étonné de trouver autour de moi une obscurité, douce et reposante pour mes yeux, mais peut-être plus encore pour mon esprit, à qui elle apparaissait comme une chose sans cause, incompréhensible, comme une chose vraiment obscure.`;
+    });
+
     it(`devrait retourner un texte sur plusieurs ligne`, () => {
-      //
+      const mockResult = 
+`Il me semblait que j’étais moi-même ce dont parlait l’ouvrage: une église, un
+quatuor, la rivalité de François Ier et de Charles-Quint.
+
+Cette croyance survivait pendant quelques secondes à mon réveil; elle ne
+choquait pas ma raison, mais pesait comme des écailles sur mes yeux et les
+empêchait de se rendre compte que le bougeoir n’était plus allumé.
+Puis elle commençait à me devenir inintelligible, comme après la métempsycose
+les pensées d’une existence antérieure; le sujet du livre se détachait de moi,
+j’étais libre de m’y appliquer ou non; aussitôt je recouvrais la vue et j’étais
+bien étonné de trouver autour de moi une obscurité, douce et reposante pour mes
+yeux, mais peut-être plus encore pour mon esprit, à qui elle apparaissait comme
+une chose sans cause, incompréhensible, comme une chose vraiment obscure.`;
+
+        const result = wrapText(text, TextOptions.MaxLine);
+        expect(result).toBe(mockResult);
     });
   });
 });

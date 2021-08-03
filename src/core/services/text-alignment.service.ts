239b1c8bf0bbd5e88d970e1fff73fe141b7f923c
WILLY Falone <willyfkouadio@gmail.com>
2021-08-03 02:25:11 +0200

    wip(text-alignment): implemente justify()

--- src/core/services/text-alignment.service.ts
+++ src/core/services/text-alignment.service.ts
@@ -1,8 +1,49 @@
 import { Service } from 'typedi';
+import { TextOptions } from '../utils/text-options.enum';
+import { TextWrapper } from '../utils/text-wrapper.util';
 
 @Service()
 export class TextAlignmentService {
+  private readonly maxLength = TextOptions.MaxLength;
+
   public justify(text: string): string {
-    return '';
+    const textWrapped = TextWrapper.wrap(text, this.maxLength);
+    
+    const jusitfiedText = textWrapped
+      .split(`\n`)
+      .map((line, i, lines) =>
+        line.length === this.maxLength || i + 1 === lines.length
+          ? line
+          : this.justifyLine(line),
+      )
+      .join('\n');
+
+    return jusitfiedText;
+  }
+
+  private justifyLine(text: string): string {
+    const words = text.split(` `);
+    if (words.length < 2) return text;
+
+    const totalCharactersWithoutSpace = words.join('').length;
+    const spreadSpace = this.maxLength - totalCharactersWithoutSpace;
+    const spaceBetweenWords = words.length - 1;
+
+    const averageSpaceBetweenWords = Math.trunc(
+      spreadSpace / spaceBetweenWords,
+    );
+
+    const defaultSpaceAdjustment = averageSpaceBetweenWords * spaceBetweenWords;
+    const moreSpaceIndex =
+      spaceBetweenWords - (spreadSpace - defaultSpaceAdjustment);
+
+    let jusitfiedText = words[0];
+    for (let i = 1; i <= spaceBetweenWords; i++) {
+      const moreSpace = i > moreSpaceIndex ? 1 : 0;
+      jusitfiedText +=
+        ' '.repeat(averageSpaceBetweenWords + moreSpace) + words[i];
+    }
+
+    return jusitfiedText;
   }
 }
--- test/core/services/text-alignment.service.spec.ts
+++ test/core/services/text-alignment.service.spec.ts
@@ -1,19 +1,60 @@
-describe('quand on passe un texte à justifier', () => {
+import { TextAlignmentService } from '../../../src/core/services/text-alignment.service';
+
+describe(`quand on appel "TextAlignmentService.justify()" avec un texte à justifier`, () => {
+  let textAlignmentService: TextAlignmentService;
+  beforeEach(() => {
+    textAlignmentService = new TextAlignmentService();
+  });
+
   describe('et que le texte contient un seul mot', () => {
-    //
+    it(`devrait retourner le même texte non justifié`, () => {
+      const text = 'TicTacTrip';
+      const result = textAlignmentService.justify(text);
+      expect(result).toBe(text);
+    });
   });
 
   describe('et que le texte fait moins de 80 caractères', () => {
     describe(`et que le texte est sur une ligne`, () => {
-      //
+      it(`devrait retourner le même texte non justifié`, () => {
+        const text =
+          'Cette croyance survivait pendant quelques secondes à mon réveil.';
+        const expected =
+          'Cette croyance survivait pendant quelques secondes à mon réveil.';
+        const result = textAlignmentService.justify(text);
+        expect(result).toBe(expected);
+      });
     });
 
     describe(`et que le texte est sur plusieurs lignes`, () => {
-      //
+      it(`devrait justifier le texte sauf la dernière ligne`, () => {
+        const text = `Longtemps, je me suis couché de bonne heure.
+Bonne nouvelle !`;
+        const expectedText = `Longtemps,      je      me      suis      couché      de      bonne       heure.
+Bonne nouvelle !`;
+        const result = textAlignmentService.justify(text);
+        expect(result).toBe(expectedText);
+      });
     });
   });
 
   describe(`et que le texte est sur plusieurs lignes`, () => {
-    //
+    it(`devrait justifier le texte sauf la dernière ligne`, () => {
+      const text = `Cette croyance survivait pendant quelques secondes à mon réveil; elle ne choquait pas ma raison, mais pesait comme des écailles sur mes yeux et les empêchait de se rendre compte que le bougeoir n’était plus allumé. 
+      Puis elle commençait à me devenir inintelligible, comme après la métempsycose les pensées d’une existence antérieure; le sujet du livre se détachait de moi, j’étais libre de m’y appliquer ou non; aussitôt je recouvrais la vue et j’étais bien étonné de trouver autour de moi une obscurité, douce et reposante pour mes yeux, mais peut-être plus encore pour mon esprit, à qui elle apparaissait comme une chose sans cause, incompréhensible, comme une chose vraiment obscure.`;
+      
+      const expectedText = 
+`Cette croyance survivait  pendant  quelques  secondes  à  mon  réveil;  elle  ne
+choquait pas ma raison, mais pesait comme des  écailles  sur  mes  yeux  et  les
+empêchait  de  se  rendre  compte  que  le   bougeoir   n’était   plus   allumé.
+Puis elle commençait à me devenir inintelligible, comme  après  la  métempsycose
+les pensées d’une existence antérieure; le sujet du livre se détachait  de  moi,
+j’étais libre de m’y appliquer ou non; aussitôt je recouvrais la vue et  j’étais
+bien étonné de trouver autour de moi une obscurité, douce et reposante pour  mes
+yeux, mais peut-être plus encore pour mon esprit, à qui elle apparaissait  comme
+une chose sans cause, incompréhensible, comme une chose vraiment obscure.`;
+      const result = textAlignmentService.justify(text);
+      expect(result).toBe(expectedText);
+    });
   });
 });
